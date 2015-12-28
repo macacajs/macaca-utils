4fbe2779171d32a5d8a08e94b58c758f41f86f79
xudafeng <xudafeng@126.com>
2015-12-28 14:52:15 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* ================================================================
+ * macaca-utils by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Mon Dec 28 2015 14:44:44 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+module.exports = require('./lib/macaca-utils');
