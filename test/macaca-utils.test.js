8223e92f1ae8dbe44c361de93a4ca4d42da3400d
鹤见 <zichen.zzc@alibaba-inc.com>
2016-04-29 14:47:12 +0800

    Clear comments

--- test/macaca-utils.test.js
+++ test/macaca-utils.test.js
@@ -1,19 +1,6 @@
-/* ================================================================
- * macaca-utils by xdf(xudafeng[at]126.com)
- *
- * first created at : Mon Dec 28 2015 14:44:44 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright 2013 xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
-var _ = require('..');
+var _ = require('../lib/macaca-utils');
 
 describe('test', function() {
   it('base', function() {
