167e517f6728f0dfb7b8fc2d4ad6c82503a63ae8
xudafeng <xudafeng@126.com>
2017-12-29 16:00:12 +0800

    feat: support ci

--- test/macaca-utils.test.js
+++ test/macaca-utils.test.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var assert = require('assert');
+
 var _ = require('../lib/macaca-utils');
 
 describe('test', function() {
   it('base', function() {
-    _.ipv4.should.be.ok;
+    assert.ok(_.ipv4);
   });
 });
